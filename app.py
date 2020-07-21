6821eef8b5bb890bff3db212805175f13dbbf752
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2020-07-21 10:21:39 -0400

    basic app for routing traffic to index.html

---
+++ app.py
@@ -0,0 +1,26 @@
+# Many thanks to: https://wikitech.wikimedia.org/wiki/Help:Toolforge/My_first_Flask_OAuth_tool
+
+import os
+import yaml
+
+from flask import Flask, render_template
+import flask_cors
+
+app = Flask(__name__)
+
+__dir__ = os.path.dirname(__file__)
+app.config.update(
+    yaml.safe_load(open(os.path.join(__dir__, 'default_config.yaml'))))
+try:
+    app.config.update(
+        yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+except IOError:
+    # It is ok if there is no local config file
+    pass
+
+# Enable CORS for API endpoints
+cors = flask_cors.CORS(app, resources={r'/api/*': {'origins': '*'}})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
