0365f77f6bbe962ff48ae254a5da46b93c22dcc6
Isaac Johnson <45494522+geohci@users.noreply.github.com>
2021-05-25 16:42:17 -0400

    add regions

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 WIKIPEDIA_LANGUAGE_CODES = ['aa', 'ab', 'ace', 'ady', 'af', 'ak', 'als', 'am', 'an', 'ang', 'ar', 'arc', 'ary', 'arz', 'as', 'ast', 'atj', 'av', 'avk', 'awa', 'ay', 'az', 'azb', 'ba', 'ban', 'bar', 'bat-smg', 'bcl', 'be', 'be-x-old', 'bg', 'bh', 'bi', 'bjn', 'bm', 'bn', 'bo', 'bpy', 'br', 'bs', 'bug', 'bxr', 'ca', 'cbk-zam', 'cdo', 'ce', 'ceb', 'ch', 'cho', 'chr', 'chy', 'ckb', 'co', 'cr', 'crh', 'cs', 'csb', 'cu', 'cv', 'cy', 'da', 'de', 'din', 'diq', 'dsb', 'dty', 'dv', 'dz', 'ee', 'el', 'eml', 'en', 'eo', 'es', 'et', 'eu', 'ext', 'fa', 'ff', 'fi', 'fiu-vro', 'fj', 'fo', 'fr', 'frp', 'frr', 'fur', 'fy', 'ga', 'gag', 'gan', 'gcr', 'gd', 'gl', 'glk', 'gn', 'gom', 'gor', 'got', 'gu', 'gv', 'ha', 'hak', 'haw', 'he', 'hi', 'hif', 'ho', 'hr', 'hsb', 'ht', 'hu', 'hy', 'hyw', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'ilo', 'inh', 'io', 'is', 'it', 'iu', 'ja', 'jam', 'jbo', 'jv', 'ka', 'kaa', 'kab', 'kbd', 'kbp', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'koi', 'kr', 'krc', 'ks', 'ksh', 'ku', 'kv', 'kw', 'ky', 'la', 'lad', 'lb', 'lbe', 'lez', 'lfn', 'lg', 'li', 'lij', 'lld', 'lmo', 'ln', 'lo', 'lrc', 'lt', 'ltg', 'lv', 'mai', 'map-bms', 'mdf', 'mg', 'mh', 'mhr', 'mi', 'min', 'mk', 'ml', 'mn', 'mnw', 'mr', 'mrj', 'ms', 'mt', 'mus', 'mwl', 'my', 'myv', 'mzn', 'na', 'nah', 'nap', 'nds', 'nds-nl', 'ne', 'new', 'ng', 'nl', 'nn', 'no', 'nov', 'nqo', 'nrm', 'nso', 'nv', 'ny', 'oc', 'olo', 'om', 'or', 'os', 'pa', 'pag', 'pam', 'pap', 'pcd', 'pdc', 'pfl', 'pi', 'pih', 'pl', 'pms', 'pnb', 'pnt', 'ps', 'pt', 'qu', 'rm', 'rmy', 'rn', 'ro', 'roa-rup', 'roa-tara', 'ru', 'rue', 'rw', 'sa', 'sah', 'sat', 'sc', 'scn', 'sco', 'sd', 'se', 'sg', 'sh', 'shn', 'si', 'simple', 'sk', 'sl', 'sm', 'smn', 'sn', 'so', 'sq', 'sr', 'srn', 'ss', 'st', 'stq', 'su', 'sv', 'sw', 'szl', 'szy', 'ta', 'tcy', 'te', 'tet', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tpi', 'tr', 'ts', 'tt', 'tum', 'tw', 'ty', 'tyv', 'udm', 'ug', 'uk', 'ur', 'uz', 've', 'vec', 'vep', 'vi', 'vls', 'vo', 'wa', 'war', 'wo', 'wuu', 'xal', 'xh', 'xmf', 'yi', 'yo', 'za', 'zea', 'zh', 'zh-classical', 'zh-min-nan', 'zh-yue', 'zu']
-MISALIGNMENT = {}
+MISALIGNMENT_TOPICS = {}
+MISALIGNMENT_REGION = {}
 TOPIC_LBLS = {}
 
 __dir__ = os.path.dirname(__file__)
@@ -52,8 +53,15 @@ def misalignment():
 def misalignment_api():
     langs = [lang for lang in request.args.get('lang', '').lower().split('|') if validate_lang(lang)][:6]
     results = []
-    for t in sorted(MISALIGNMENT):
-        results.append({'topic':t, 'topic-display':TOPIC_LBLS[t], 'data':{l:{'num_articles':MISALIGNMENT[t][l][0], 'misalignment':f'{MISALIGNMENT[t][l][1]:.3f}'} for l in langs}})
+    if request.args.get('facet', '') == 'geo':
+        for r in sorted(MISALIGNMENT_REGION):
+            results.append({'topic': r, 'topic-display': r,
+                            'data': {l: {'num_articles': MISALIGNMENT_REGION[r][l][0], 'misalignment': f'{MISALIGNMENT_REGION[r][l][1]:.3f}'} for l in langs}})
+    else:
+        for t in sorted(MISALIGNMENT_TOPICS):
+            results.append({'topic': t, 'topic-display': TOPIC_LBLS[t],
+                            'data': {l: {'num_articles': MISALIGNMENT_TOPICS[t][l][0], 'misalignment': f'{MISALIGNMENT_TOPICS[t][l][1]:.3f}'} for l in langs}})
+
     return jsonify({'langs':langs, 'results':results})
 
 
@@ -80,7 +88,7 @@ def validate_api_args():
 
     return lang, title, qid
 
-def load_misalignment_data():
+def load_misalignment_topic_data():
     misalignment_url = 'https://analytics.wikimedia.org/published/datasets/one-off/isaacj/misalignment/misalignment-by-wiki-topic.tsv.gz'
     misalignment_fn = misalignment_url.split('/')[-1]
     if not os.path.exists(misalignment_fn):
@@ -102,15 +110,46 @@ def load_misalignment_data():
             topic = line[topic_idx]
             topic_lbl = line[top_lbl_idx]
             TOPIC_LBLS[topic] = topic_lbl
-            if topic not in MISALIGNMENT:
-                MISALIGNMENT[topic] = {}
+            if topic not in MISALIGNMENT_TOPICS:
+                MISALIGNMENT_TOPICS[topic] = {}
             count = int(line[count_idx])
             mis = float(line[mis_idx])
-            MISALIGNMENT[topic][wiki] = (count, mis)
+            MISALIGNMENT_TOPICS[topic][wiki] = (count, mis)
 
-    for topic in MISALIGNMENT:
+    for topic in MISALIGNMENT_TOPICS:
         for w in wikis:
-            if w not in MISALIGNMENT[topic]:  # possibly define a minimum sample size here too -- e.g., 30
-                MISALIGNMENT[topic][w] = (0, '--')
+            if w not in MISALIGNMENT_TOPICS[topic]:  # possibly define a minimum sample size here too -- e.g., 30
+                MISALIGNMENT_TOPICS[topic][w] = (0, '--')
 
-load_misalignment_data()
+def load_misalignment_geo_data():
+    misalignment_url = 'https://analytics.wikimedia.org/published/datasets/one-off/isaacj/misalignment/misalignment-by-wiki-region.tsv.gz'
+    misalignment_fn = misalignment_url.split('/')[-1]
+    if not os.path.exists(misalignment_fn):
+        urlretrieve(misalignment_url, misalignment_fn)
+    expected_header = ['wiki_db', 'region', 'num_articles', 'avg_misalignment']
+    region_idx = expected_header.index('region')
+    wiki_idx = expected_header.index('wiki_db')
+    count_idx = expected_header.index('num_articles')
+    mis_idx = expected_header.index('avg_misalignment')
+    wikis = set()
+    with gzip.open(misalignment_fn, 'rt') as fin:
+        header = next(fin).strip().split('\t')
+        assert header == expected_header
+        for line in fin:
+            line = line.strip().split('\t')
+            wiki = line[wiki_idx]
+            wikis.add(wiki)
+            region = line[region_idx]
+            if region not in MISALIGNMENT_REGION:
+                MISALIGNMENT_REGION[region] = {}
+            count = int(line[count_idx])
+            mis = float(line[mis_idx])
+            MISALIGNMENT_REGION[region][wiki] = (count, mis)
+
+    for region in MISALIGNMENT_REGION:
+        for w in wikis:
+            if w not in MISALIGNMENT_REGION[region]:  # possibly define a minimum sample size here too -- e.g., 30
+                MISALIGNMENT_REGION[region][w] = (0, '--')
+
+load_misalignment_topic_data()
+load_misalignment_geo_data()
